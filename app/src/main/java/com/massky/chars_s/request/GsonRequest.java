f824ce3b6a63b0482f31c4454ed0301a0ffc37c9
zhu <zhu@gmail.com>
2020-05-27 17:25:27 +0800

    2020 - 5 -27 Android Volley完全解析(四)，带你从源码的角度理解Volley-read over

--- app/src/main/java/com/massky/chars_s/request/GsonRequest.java
+++ app/src/main/java/com/massky/chars_s/request/GsonRequest.java
@@ -5,6 +5,7 @@ import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.google.gson.Gson;
 
 import java.io.UnsupportedEncodingException;
 
